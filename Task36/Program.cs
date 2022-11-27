ba45499dbe516a1e3dfcd7957f6d83ec3518e931
TayaL <tayalyapina555@gmail.com>
2022-11-27 13:25:33 +0300

    Task 34, task 36 and task 38 added

---
+++ Task36/Program.cs
@@ -0,0 +1,40 @@
+﻿/*Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0
+*/
+
+Console.Write("Введите количество элементов в массиве: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+int[] numbers = new int[size];
+
+FillArrayRandomNumbers(numbers, -99, 99);
+WriteArray(numbers);
+
+int sum = 0;
+
+for(int i = 0; i < numbers.Length; i+=2)
+{
+        sum = sum + numbers[i];
+}
+
+Console.WriteLine($"Сумма элементов, стоящих на нечетных позициях: {sum}");
+
+void FillArrayRandomNumbers(int[] array, int min, int max)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(min, max + 1);
+    }
+}
+
+void WriteArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
---
+++ Task38/Program.cs
@@ -0,0 +1,49 @@
+﻿/*Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+[3 7 22 2 78] -> 76
+
+*/
+
+Console.Write("Введите количество элементов в массиве: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+double[] numbers = new double[size];
+
+FillArrayRandomNumbers(numbers, 0, 99);
+
+WriteArray(numbers);
+
+double minimum = numbers[0];
+double maximum = numbers[0];
+
+for(int i = 0; i < numbers.Length; i++)
+{
+    if (numbers[i]> maximum)
+        maximum = numbers[i];
+    else if (numbers[i] < minimum)
+        minimum = numbers[i];
+}
+
+double result = maximum - minimum;
+
+Console.WriteLine($" -> {result}");
+
+
+
+void FillArrayRandomNumbers(double[] array, int min, int max)
+{
+for(int i = 0; i < array.Length; i++)
+{
+array[i] = new Random().Next(min, max + 1)+ new Random().NextDouble();
+}
+}
+
+void WriteArray(double[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+   
+}
+
